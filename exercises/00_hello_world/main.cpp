39002f91190a82b917cb737f7671b6b0c7487bc2
yinbing22 <3085749051@qq.com>
2025-01-03 17:14:03 +0800

    10

--- exercises/00_hello_world/main.cpp
+++ exercises/00_hello_world/main.cpp
@@ -3,7 +3,7 @@
 // READ: std streams <https://zh.cppreference.com/w/cpp/io/c/std_streams>
 // READ: 流修饰符 <https://zh.cppreference.com/w/cpp/io/manip>
 // READ: format in cxx20 <https://zh.cppreference.com/w/cpp/utility/format/format>
-
+//sucess
 int main(int argc, char **argv) {
     // TODO: 在控制台输出 "Hello, InfiniTensor!" 并换行
     std::cout << "Hello, InfiniTensor!" << std::endl;
